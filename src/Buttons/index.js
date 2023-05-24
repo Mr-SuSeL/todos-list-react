512a0dfe263cf9a30683ca509ddb258811748adb
Łukasz Susłowicz <susel.invest@gmail.com>
2023-05-24 17:31:56 +0100

    BEM and js- debug

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,20 +1,20 @@
 import "./style.css";
 
-const Buttons = (props) => {
-    if (props.tasks.length === 0) {
+const Buttons = ({ tasks, hideDoneTasks }) => {
+    if (tasks.length === 0) {
         return null;
     }
 
     return (
-        <div className="js-tasksButtons gridButtons">
+        <div className="gridButtons">
 
-            <button className="js-buttonHideDone section__heading__hideButton">
-                {props.hideDoneTasks ? "Pokaż " : "Ukryj "}
+            <button className="section__heading--hideButton">
+                {hideDoneTasks ? "Pokaż " : "Ukryj "}
                 ukończone zadania
             </button>
             <button
-                className="js-buttonFinishAll section__heading__finishButton"
-                disabled={props.tasks.every(({ done }) => done)}
+                className="section__heading--finishButton"
+                disabled={tasks.every(({ done }) => done)}
 
             >
                 Zakończ wszystkie zadania
