1165131d3c5ec7839b417b86c60a292d22c11644
Łukasz Susłowicz <susel.invest@gmail.com>
2023-09-07 14:17:43 +0100

    Removed classes with "js-" entry

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const Form = () => (
     <form className="form">
-        <input className="js-newTask flex__item" placeholder="Co jest do zrobienia?" autofocus />
+        <input className="newTask flex__item" placeholder="Co jest do zrobienia?" autofocus />
         <button className="button__task flex__item">Dodaj zadanie</button>
     </form>
 );
