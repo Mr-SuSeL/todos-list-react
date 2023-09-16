2a03b9192645e6c3bd0ef7e601ed469e545cb5af
Łukasz Susłowicz <susel.invest@gmail.com>
2023-09-16 15:25:19 +0100

    task section re-styled

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,35 +1,41 @@
-import "./style.css";
-import { List } from "./styled";
+// import "./style.css";
+import { List, Item, Content, Button } from "./styled";
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-  
-  // <List className="tasks">
+ 
   <List>
     {tasks.map((task) => (
-      <li
+      <Item
         key={task.id}
-        className={`
-      "tasks__item${task.done ? " tasks__item--done" : ""} tasks__item${
-          task.done && hideDone ? " tasks__item--done tasks__item--hidden" : ""
-        }
-      `}
+        hidden={task.done && hideDone}
+
+      //   className={`
+      // "tasks__item${task.done ? " tasks__item--done" : ""} tasks__item${
+      //     task.done && hideDone ? " tasks__item--done tasks__item--hidden" : ""}`
+        
+        // }
       >
-        <button
-          className="content__item content__item--left"
+        <Button
+          toggleDone
           onClick={() => toggleTaskDone(task.id)}
         >
           {task.done ? "✔" : ""}
-        </button>
-        <div className="content__item content__item--center">
+        </Button>
+        
+        {/* <span className="content__item--center"> */}
+        <Content done={task.done}>
           {task.content}
-        </div>
-        <button
-          className="content__item content__item--right"
+        </Content>
+
+        {/* </span> */}
+
+        <Button
+          remove
           onClick={() => removeTask(task.id)}
         >
           🗑
-        </button>
-      </li>
+        </Button>
+      </Item>
     ))}
   </List>
 );
--- src/Tasks/styled.js
+++ src/Tasks/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const List = styled.ul`
     background-color: #fff;
@@ -9,4 +9,78 @@ export const List = styled.ul`
     border-top: 0;
     list-style: none;
     padding: 0px;
+`;
+
+export const Item = styled.li`
+    display: grid;
+    grid-template-columns: 70px 1fr 70px;
+    padding: 20px;
+    margin: 0 20px 10px 10px;
+    border-bottom: 2px solid #ddd;
+    text-align: left;
+    /* margin-left: -20px; */
+    align-items: center;
+
+    ${(props) => props.hidden && css`
+        display: none;
+    `}
+`;
+
+export const Content = styled.span`
+    ${({ done }) => done && css`
+         text-decoration: line-through;
+         display: flex;
+         align-items: center;
+         /* margin-left: -10px; */
+
+        /* display: flex;
+        align-items: center;
+
+        justify-content: left;
+        padding-left: 0;
+        margin-left: -10px;
+
+        padding-right: 0;
+        align-self: left; */
+    `}
+`;
+
+export const Button = styled.button`
+    display: flex;
+    align-items: center;
+    /* text-align: left; */
+    /* justify-content: center; */
+        text-decoration: none;
+    /* margin-left: 30px; */
+        width: 30px;
+        height: 30px;
+        color: white;
+        cursor: pointer;
+        border: none;
+        transition: background 0.3s;
+
+    ${({ toggleDone }) => toggleDone && css`
+        background-color: hsl(122, 59%, 40%);
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &: hover {
+            background-color: hsl(122, 59%, 50%);
+        }
+    `}
+
+    ${({ remove }) => remove && css`
+        background-color: hsl(0, 89%, 42%);
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &: hover {
+            background-color: hsl(0, 89%, 52%);
+        }
+    `}
+
 `;
