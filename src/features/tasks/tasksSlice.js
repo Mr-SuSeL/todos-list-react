2119d2fed2be6efb469ac1612e14e512f2f12ce4
Łukasz Susłowicz <susel.invest@gmail.com>
2025-11-01 15:57:35 +0100

    redux store initialized

---
+++ src/features/tasks/tasksSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState: {
+        tasks: [],
+    },
+    reducers: {
+        addTask: ({ tasks }, { payload }) => {
+            tasks.push(payload); // immer library here allows us to mutate-like state directly
+        },
+        removeTask: ({ tasks }, { payload }) => {
+            return {
+                tasks: tasks.filter(task => task.id !== payload),
+            };
+        },
+    },
+}); 
+
+export const { addTask, removeTask } = tasksSlice.actions;
+export default tasksSlice.reducer;
+export const selectTasks = state => state.tasks;
+
+// console.log(addTask({
+//     content: "Nauczys się Reduxa",
+//     done: false,
+//     id: 5
+// }));
+
+// console.log(tasksSlice.reducer({ tasks: [] }, addTask({
+//     content: "Nauczys się Reduxa",
+//     done: true,
+//     id: 5
+// })));
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GlobalStyle from './GlobalStyle';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <GlobalStyle />
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
